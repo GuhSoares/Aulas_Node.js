87273f60c2a823cf2bdba43895d943aba5c85dcc
Gustavo Soares <Gustavohsoaressilva@gmail.com>
2022-06-12 19:07:39 -0300

    Segunda Atividade Finalizada

---
+++ Atividades/2_Segunda_atividade/index.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer")
+const chalk = require("chalk")
+
+inquirer.prompt([
+    {name: 'nome', message: "Qual o seu nome?"},
+    {name: 'idade', message: 'Qual a sua idade?'},
+])
+.then((answers) => {
+    const response = `O nome dele é ${answers.nome} e ele tem ${answers.idade} anos!`
+    console.log(chalk.bgYellow.black(response))
+})
+.catch((err) => console.log(err))
