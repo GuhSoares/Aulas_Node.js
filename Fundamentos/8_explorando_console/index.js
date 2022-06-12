d0d2b259e30d2c326340b8a629736c1b936616cb
Gustavo Soares <Gustavohsoaressilva@gmail.com>
2022-06-12 18:52:56 -0300

    Fundamentos Node.js

---
+++ Fundamentos/8_explorando_console/index.js
@@ -0,0 +1,21 @@
+// mais de um valor
+const x = 10
+const y = 'Paulo'
+const z = [1, 2]
+
+console.log(x, y ,z)
+
+// contagem de impressões
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+
+// variavel entre string
+console.log("o nome é %s, ele é programador", y)
+
+// limpar o console
+setTimeout(() => {
+    console.clear()
+}, 2000);
