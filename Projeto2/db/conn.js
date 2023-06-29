d9466bed046ec496fb726cb063bcff02c933ecfe
Gustavo Soares <gustavohsoaressilva@hens.com.br>
2023-06-29 17:15:20 -0300

    Projeto 2 TOUGHTS Finalizada aula 178

---
+++ Projeto2/db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('projeto2', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (err) {
+    console.error(`Unable to connect to the database:, ${err}`)
+}
+
+module.exports = sequelize;
