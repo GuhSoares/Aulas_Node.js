3e96e1b7af3fc61a64c7726f978f776fe4517b0c
Gustavo Soares <gustavohsoaressilva@hens.com.br>
2023-07-19 15:19:08 -0300

    Criada a navegação do site

--- get_a_pet/frontend/src/App.js
+++ get_a_pet/frontend/src/App.js
@@ -1,9 +1,27 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+/* components */
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+import Container from './components/layouts/Container';
+/* pages */
+import Home from './components/pages/Auth/Home';
+import Login from './components/pages/Auth/Login';
+import Register from './components/pages/Auth/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Get a Pet</h1>
-    </div>
+    <Router>
+    <Navbar />
+    <Container>
+    <Routes> 
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </Container>
+    <Footer />
+    </Router>
   );
 }
 
