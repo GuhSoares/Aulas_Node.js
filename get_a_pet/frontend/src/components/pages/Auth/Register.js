57988821cced1528f9c63aca557ddf4c698f7a4e
Gustavo Soares <gustavohsoaressilva@hens.com.br>
2023-07-20 14:03:04 -0300

    Finalizado css da pagina register

--- get_a_pet/frontend/src/components/pages/Auth/Register.js
+++ get_a_pet/frontend/src/components/pages/Auth/Register.js
@@ -1,7 +1,53 @@
+import Input from '../../form/Input.js'
+import styles from '../../form/Form.module.css'
+
 function Register() {
+
+    function handleOnChange(e) {}
     return(
-        <section>
-            <h1>Register</h1>
+        <section className={styles.form_container}>
+            <h1>Registrar</h1>
+            <form>
+             <Input 
+             text="Nome"
+                type="text"
+                name="name"
+                placeholder="Digite seu nome"
+                handleOnChange={handleOnChange}
+             />
+             <Input 
+             text="Telefone"
+                type="text"
+                name="phone"
+                placeholder="Digite seu telefone"
+                handleOnChange={handleOnChange}
+             />
+             <Input 
+             text="E-mail"
+                type="email"
+                name="email"
+                placeholder="Digite seu E-mail"
+                handleOnChange={handleOnChange}
+             />
+             <Input 
+             text="Senha"
+                type="password"
+                name="password"
+                placeholder="Digite sua senha"
+                handleOnChange={handleOnChange}
+             />
+             <Input 
+             text="Confirmação de Senha"
+                type="password"
+                name="confirmpassword"
+                placeholder="Confirme sua senha"
+                handleOnChange={handleOnChange}
+             />
+             <input type="submit" value="Cadastrar"/>
+            </form>
+            <p>
+                Já tem uma conta?<a href="/login">Clique Aqui!</a>
+            </p>
         </section>
     )
     }
