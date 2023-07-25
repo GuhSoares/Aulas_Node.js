21ed25d65130a9c41c9b2e19fd267ff376dfa22a
Gustavo Soares <gustavo.silva@hens.com.br>
2023-07-25 17:20:08 -0300

    add novas rotas de edit,Details,Adoptions

--- get_a_pet/frontend/src/App.js
+++ get_a_pet/frontend/src/App.js
@@ -13,6 +13,10 @@ import Register from './components/pages/Auth/Register';
 import Profile from './components/pages/User/Profile';
 import MyPets from './components/pages/Pet/MyPets';
 import AddPet from './components/pages/Pet/AddPet'
+import EditPet from './components/pages/Pet/EditPet'
+import PetDatails from './components/pages/Pet/PetDatails'
+import MyAdoptions from './components/pages/Pet/MyAdoptions'
+
 /* context */
 import { UserProvider } from './context/UserContext';
 
@@ -29,6 +33,9 @@ function App() {
       <Route path="/user/profile" element={<Profile />} />
       <Route path="/pet/mypets" element={<MyPets />} />
       <Route path="/pet/add" element={<AddPet />} />
+      <Route path="/pet/edit/:id" element={<EditPet />} />
+      <Route path="/pet/myadoptions" element={<MyAdoptions />} />
+      <Route path="/pet/:id" element={<PetDatails />} />
       <Route path="/" element={<Home />} />
     </Routes>
     </Container>
